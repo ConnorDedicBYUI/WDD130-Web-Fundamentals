64a9f0e78c2de0e87f6808e8826459c16cfa4bc5
ConnorDedicBYUI <163693596+ConnorDedicBYUI@users.noreply.github.com>
2024-03-16 17:47:15 -0600

    Add files via upload
    duplicated repo

---
+++ WDD (web fund) 130/my site/js/myScript.js
@@ -0,0 +1,22 @@
+function openPic(event, picName) {
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(picName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+
+  //This code is a placeholder until i write my own code for this. it is based of the w3c schools js for tab menus
